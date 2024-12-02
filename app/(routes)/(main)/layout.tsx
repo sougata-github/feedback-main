2f458edf4dc15970ee7523c23df79cda2f86b8a0
Sougata <sougatadas9874@gmail.com>
2024-12-02 19:17:12 +0530

    modify: main layout padding.

--- app/(routes)/(main)/layout.tsx
+++ app/(routes)/(main)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <MainSidebar projects={projects || []} />
-      <main className="overflow-x-hidden overflow-y-auto flex-1 min-h-screen pb-8 md:pb-12 pt-6 md:pt-8 px-4 md:px-8 relative">
+      <main className="overflow-x-hidden overflow-y-auto flex-1 min-h-screen pb-8 md:pb-12 pt-14 md:pt-16 px-4 md:px-8 relative">
         <div className="md:flex-1">{children}</div>
         <SidebarTrigger className="absolute top-8 md:top-12 right-4 md:right-8" />
       </main>
