446501f5f13f65a8e80b6ac93e430cbb32c94d44
Sougata <sougatadas9874@gmail.com>
2024-12-09 20:05:43 +0530

    change pricing page title and make it protected route.

--- app/(routes)/(main)/(payments)/pricing/page.tsx
+++ app/(routes)/(main)/(payments)/pricing/page.tsx
@@ -3,7 +3,7 @@ import PageTitle from "@/components/main/PageTitle";
 const page = () => {
   return (
     <section>
-      <PageTitle title="Pricing" />
+      <PageTitle title="Choose the plan that fits your needs" />
 
       <div className="mt-12"></div>
     </section>
