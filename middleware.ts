028a6bf988b8d7aea658eb52f7166e731565bf95
Sougata <sougatadas9874@gmail.com>
2024-12-10 19:13:15 +0530

    protect payment routes.

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/projects(.*)",
+  "/pricing(.*)",
+  "/subscriptions(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
